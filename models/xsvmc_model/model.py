503763e5e093b9cdb6ee82c3ba7166efb4dc7533
Jonathan García <jonfrgar@espol.edu.ec>
2023-01-15 10:54:54 -0500

    Fixed model

--- models/xsvmc_model/model.py
+++ models/xsvmc_model/model.py
@@ -4,13 +4,15 @@ from joblib import load
 from transformers import BertTokenizer
 from keras.utils import pad_sequences
 from xsvmlib.xsvmc import xSVMC
+import base64
+from io import BytesIO
 
 max_len = 4433
     
 clf = load("models/xsvmc_model/xsvmc.joblib")
 SVs = clf.support_vectors_
 
-tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-cased', do_lower_case=True)
+tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-cased', do_lower_case=False)
 
 colormap = LinearSegmentedColormap.from_list('custom', 
                                        [(0, '#162cd9'),
@@ -20,7 +22,7 @@ def draw_text(draw_obj, text, pos_x, pos_y, prob, font):
   color = colormap(prob)[:3]
   draw_obj.text((pos_x, pos_y), text, fill=(int(color[0] * 255), int(color[1] * 255), int(color[2] * 255), 255), font=font)
 
-def create_img(l_text, prob, path, y_size):
+def create_img(l_text, prob, y_size):
   offset_y = 10
   offset_x = 10
   img_x = 800
@@ -52,19 +54,22 @@ def create_img(l_text, prob, path, y_size):
     last_pos_x += pos_x
 
   combined = Image.alpha_composite(img, txt)
-  combined.save(path)
+  buffered = BytesIO()
+  combined.save(buffered, format="PNG")
+  img_str = base64.b64encode(buffered.getvalue())
+  return img_str
 
-def render_text(text, misv, save_name):
+def render_text(text, misv):
   y_size = int(len(text) / 60 * 25)
   values = misv
-  max_value = max_value(values)
+  max_value = max(values)
   prob = (values / max_value)
-  create_img(text, prob, "./%s.png" % save_name, y_size)
+  create_img(text, prob, y_size)
 
 def preprocess_text(text):
   encoded_input = tokenizer(text, return_tensors='tf')
   tokens = encoded_input.input_ids[0]
-  ready_tokens = pad_sequences(tokens, padding="post", maxlen=max_len)[0]
+  ready_tokens = pad_sequences([tokens], padding="post", maxlen=max_len)[0]
   text_tokens = tokenizer.convert_ids_to_tokens(ready_tokens)
   return ready_tokens, text_tokens, len(tokens)
 
@@ -72,9 +77,17 @@ def contextualized_prediction(text):
   tokens, text_tokens, original_len = preprocess_text(text)
   clean_text = text_tokens[1:original_len-1]
   topK = clf.predict_with_context(tokens)
+  response = []
   for i in range(len(topK)):
     pred = topK[i]
     mu_misv = SVs[pred.eval.mu_hat.misv_idx][1:original_len-1]
     nu_misv = SVs[pred.eval.nu_hat.misv_idx][1:original_len-1]
-    render_text(clean_text, mu_misv, "%d-favor" % i)
-    render_text(clean_text, nu_misv, "%d-contra" % i)
+    b64_pro = render_text(clean_text, mu_misv)
+    b64_contra = render_text(clean_text, nu_misv)
+    print(b64_contra)
+    response.append({
+      'clase': pred.class_name,
+      'favor': b64_pro,
+      'contra': b64_contra
+    })
+  return response
